5a63a5972f0569d4c8d5862bd3a6115e679cad42
RoxanneGutierrez <roxannenguyenn@gmail.com>
2022-11-18 20:02:36 -0600

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    handleToDo(event.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function handleToDo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = " x ";
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.getElementById("list").appendChild(p);
+}
+
+function handleDelete(event) {
+  event.target.parentNode.remove();
+}
